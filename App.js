577051d733dc1e1c66d04bc7bb04e92704021552
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-05-31 16:07:16 -0700

    Implement demo for haptics vibration

--- App.js
+++ App.js
@@ -1,11 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Haptics from 'expo-haptics';
 
 export default function App() {
+  const [massageOn, setMassgeOn] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Phone Massager</Text>
+      <View style={styles.button}>
+        <Button
+          title="Level 1"
+          onPress={() => {
+            Haptics.notificationAsync(
+              Haptics.NotificationFeedbackType.Success
+            );
+          }}
+        />
+      </View>
+
+      <View style={styles.button}>
+        <Button
+          title="Level 2"
+          style={styles.button}
+          onPress={() => {
+            Haptics.notificationAsync(
+              Haptics.NotificationFeedbackType.Error
+            );
+          }}
+        />
+      </View>
+
+      <View>
+        <Button
+          title="Level 3"
+          onPress={() => {
+            Haptics.notificationAsync(
+              Haptics.NotificationFeedbackType.Warning
+            );
+          }}
+        />
+      </View>
+
+      <View>
+        <Button
+          title="Level 4"
+          onPress={() => {
+            Haptics.impactAsync(
+              Haptics.ImpactFeedbackStyle.Light
+            );
+          }}
+        />
+      </View>
+
+      <View>
+        <Button
+          title="Level 5"
+          onPress={() => {
+            Haptics.impactAsync(
+              Haptics.ImpactFeedbackStyle.Medium
+            );
+          }}
+        />
+      </View>
+
+
+      <View>
+        <Button
+          title="Level 6"
+          onPress={() => {
+            Haptics.impactAsync(
+              Haptics.ImpactFeedbackStyle.Heavy
+            );
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -17,4 +86,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 20,
+    marginBottom: 20
+  },
+  button: {
+    marginBottom: 15,
+  }
 });

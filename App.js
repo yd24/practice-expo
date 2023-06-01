87073e73700c4f63da8ece95fb5a81f9e0b0c377
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-05-31 18:43:06 -0700

    Add different vibration patterns

--- App.js
+++ App.js
@@ -1,81 +1,101 @@
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import * as Haptics from 'expo-haptics';
+import { useState, useEffect } from 'react';
+import { Button, StyleSheet, Text, View, Vibration } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
 
 export default function App() {
-  const [massageOn, setMassgeOn] = useState(false);
+  const [massageOn, setMassageOn] = useState(false);
+  const [massageType, setMassageType] = useState(null);
+
+  const massage = (action) => {
+    switch(action) {
+      case 'pulse':
+        Vibration.vibrate([1000, 500], true);
+        break;
+      case 'fast-pulse':
+        Vibration.vibrate([50, 50], true);
+        break;
+      case 'wave':
+        Vibration.vibrate([100, 600, 80, 400, 60, 300, 50, 200, 40, 100, 30, 50, 30, 50, 40, 100, 50, 200, 60, 300, 80, 400], true);
+        break;
+      case 'steady':
+        Vibration.vibrate([0, 500], true);
+        break;
+      default:
+        Vibration.vibrate([0, 500], true);
+        break;
+    }
+  };
+
+  useEffect(() => {
+    if (massageOn) {
+      massage(massageType);
+    } else {
+      Vibration.cancel();
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [massageOn])
+
+  useEffect(() => {
+    if (massageOn) {
+      massage(massageType);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [massageType])
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Phone Massager</Text>
-      <View style={styles.button}>
-        <Button
-          title="Level 1"
-          onPress={() => {
-            Haptics.notificationAsync(
-              Haptics.NotificationFeedbackType.Success
-            );
-          }}
-        />
-      </View>
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <Text style={styles.title}>Phone Massager</Text>
 
-      <View style={styles.button}>
-        <Button
-          title="Level 2"
-          style={styles.button}
-          onPress={() => {
-            Haptics.notificationAsync(
-              Haptics.NotificationFeedbackType.Error
-            );
-          }}
-        />
-      </View>
+        <Text>{`${massageOn}`}</Text>
+        <Text>{`${massageType}`}</Text>
 
-      <View>
-        <Button
-          title="Level 3"
-          onPress={() => {
-            Haptics.notificationAsync(
-              Haptics.NotificationFeedbackType.Warning
-            );
-          }}
-        />
-      </View>
+        <View style={styles.button}>
+          <Button
+            title={massageOn ? 'Turn off' : 'Turn on'}
+            onPress={() => {
+              setMassageOn(!massageOn);
+            }}
+          />
+        </View>
 
-      <View>
-        <Button
-          title="Level 4"
-          onPress={() => {
-            Haptics.impactAsync(
-              Haptics.ImpactFeedbackStyle.Light
-            );
-          }}
-        />
-      </View>
+        <View style={styles.button}>
+          <Button
+            title="Level 1"
+            onPress={() => {
+              setMassageType('pulse');
+            }}
+          />
+        </View>
 
-      <View>
-        <Button
-          title="Level 5"
-          onPress={() => {
-            Haptics.impactAsync(
-              Haptics.ImpactFeedbackStyle.Medium
-            );
-          }}
-        />
-      </View>
+        <View style={styles.button}>
+          <Button
+            title="Level 2"
+            onPress={() => {
+              setMassageType('fast-pulse');
+            }}
+          />
+        </View>
 
+        <View style={styles.button}>
+          <Button
+            title="Level 3"
+            onPress={() => {
+              setMassageType('wave');
+            }}
+          />
+        </View>
 
-      <View>
-        <Button
-          title="Level 6"
-          onPress={() => {
-            Haptics.impactAsync(
-              Haptics.ImpactFeedbackStyle.Heavy
-            );
-          }}
-        />
+        <View style={styles.button}>
+          <Button
+            title="Level 4"
+            onPress={() => {
+              setMassageType('steady');
+            }}
+          />
+        </View>
       </View>
-    </View>
+    </NativeBaseProvider>
   );
 }
 
